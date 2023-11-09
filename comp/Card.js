7bbee1fe6f26314feaff239885fdb71b6ea0e2c0
anjufehno <lizakrug@tlu.ee>
2023-11-09 19:10:10 +0200

    muudatus

---
+++ comp/Card.js
@@ -0,0 +1,41 @@
+function Card(props) {
+  const { Title, Year, imdbID, Type, Poster } = props;
+  const text = Title.replace(/^a-z0-9 /i, "").replace(/\s/, "+");
+  return (
+    <div id={"movie-" + imdbID} className="card">
+      <div className="card-image waves-effect waves-block waves-light">
+        {Poster !== "N/A" ? (
+          <img className="activator" src={Poster} alt="" />
+        ) : (
+          <img
+            className="activator"
+            src={`https://via.placeholder.com/300x430.png?text=${text}`}
+            alt=""
+          />
+        )}
+      </div>
+      <div className="card-content">
+        <span className="card-title activator black-text text-darken-4">
+          {Title}
+        </span>
+        <p>
+          <span>
+            {Year}, {Type}
+          </span>
+          <h
+            href="#"
+            className="right"
+            onClick={(event) => {
+              event.preventDefault();
+              props.readMoreHandler(imdbID);
+            }}
+          >
+            Read more...
+          </h>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- comp/Films.js
+++ comp/Films.js
@@ -1,8 +1,15 @@
+import Card from "./Card";
 function Films(props) {
   return (
     <div className="films">
       {props.films.length ? (
-        props.films.map((movie) => <div />)
+        props.films.map((movie) => (
+          <Card
+            key={movie.imdbID}
+            readMoreHandler={props.readMoreHandler}
+            {...movie}
+          />
+        ))
       ) : (
         <p>Nothing found</p>
       )}
